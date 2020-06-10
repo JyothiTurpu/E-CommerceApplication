2fbddbe764b5135c80752b0ded34756f2436d770
Jyothi Turpu <jyothi.turpu@gmail.com>
2020-06-10 15:09:20 -0400

    Styling SearchBar

--- src/components/shop/shopSearchBar.js
+++ src/components/shop/shopSearchBar.js
@@ -3,8 +3,12 @@ import { reduxForm, Field } from 'redux-form';
 
 function FormSearchBar(props) {
   return (
-    <input className={`${props.className} form-search-bar`} type="text" {...props.input} 
-    placeholder={`${props.placeholder}`}/>
+    <div className='search-bar-grid'>
+       <i className="fas fa-search search-bar-grid__icon"></i>
+       <input className={`${props.className} form-search-bar search-bar-grid__input`} type="text" {...props.input} 
+              placeholder={`${props.placeholder}`}/>
+    </div>
+    
   )
 }
 
@@ -13,7 +17,7 @@ class ShopSearchBar extends Component {
     const { className, handleSubmit } = this.props;
     return (
       <form onSubmit={handleSubmit} className={`${className} shop-search-bar`}>
-        <Field name='query' className='shop-search-bar__form-search-bar' placeholder='search'  component={FormSearchBar}/>
+        <Field name='query' className='shop-search-bar__form-search-bar' placeholder='Search'  component={FormSearchBar}/>
       </form>
     )
   }
