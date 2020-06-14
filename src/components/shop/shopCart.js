a9af257f9fc14c2b898714565fa29c3d38409d9a
Jyothi Turpu <jyothi.turpu@gmail.com>
2020-06-13 21:16:26 -0400

    Styling CartFooter Component

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -11,18 +11,36 @@ function CartButton({className}) {
 
 function CartContent({className, products}) {
   let count = products.length;
+  let productsJSX = products.map(product => <h1 key={product}>{product}</h1>);
 
   return (
     <div className={`${className} cart-content`}>
-      <div className="cart-content__title">
+      <a className="cart-content__title">
           Cart({count})
-      </div>
+      </a>
       <div className="cart-content__cart-products">
-
+          {productsJSX}
       </div>
-      <div className="cart-content__footer">
+      <CartFooter className='cart-content__footer' products={products} />
+    </div>
+  )
+}
 
-      </div>
+
+function CartFooter({className, products}) {
+  const price = '7.96';
+
+  return (
+    <div className={`${className} cart-footer`}>
+        <button className='cart-footer__checkout'>
+          Checkout
+        </button>
+        <div className="cart-footer__subtotal">
+          SubTotal
+        </div>
+        <div className="cart-footer__price">
+          ${price}
+        </div>
     </div>
   )
 }
